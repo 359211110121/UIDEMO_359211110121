89d88402fca406e27ea11c3a088061b21d47538d
LAB_COM-MT_1 <patchayaphon14@gmail.com>
2018-02-25 17:35:44 +0700

    data

--- src/dbUtil/dbconnection.java
+++ src/dbUtil/dbconnection.java
@@ -4,22 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class dbconnection {
-    private static final String SQCONN = "jdbc : sqlite : school sqlite";
-
+public class dbConnection {
+    private static final String SQCONN = "jdbc:sqlite:school.sqlite";
 
     public static Connection getConnection() throws SQLException {
-        try{
+        try {
             Class.forName("org.sqlite.JDBC");
-            return DriverManager.getConnection(SQCONN)
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-            if (this.connection == null) {
+            return DriverManager.getConnection(SQCONN);
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
         return null;
+
     }
+
+
 
 
 }//class
